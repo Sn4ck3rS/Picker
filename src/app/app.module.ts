7df487318fa238ac4d24f9ff95fe5a0bb399346d
Rezmer <paul.rezmer@atos.net>
2020-05-28 14:10:56 +0200

    routing and configure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,12 @@ import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { ClassesComponent } from './homepage/classes/classes.component';
 import { DragAndDropComponent } from './homepage/drag-and-drop/drag-and-drop.component';
-import { HeaderComponent } from './homepage/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { DragndropDirective } from './homepage/dragndrop.directive';
+import { StudentsComponent } from './classpage/students/students.component';
+import { SelectionButtonsComponent } from './classpage/selection-buttons/selection-buttons.component';
+import { HomepageComponent } from './homepage/homepage/homepage.component';
+import { ClasspageComponent } from './classpage/classpage/classpage.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { DragndropDirective } from './homepage/dragndrop.directive';
     ClassesComponent,
     DragAndDropComponent,
     HeaderComponent,
-    DragndropDirective
+    DragndropDirective,
+    StudentsComponent,
+    SelectionButtonsComponent,
+    HomepageComponent,
+    ClasspageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/classpage/classpage/classpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classpage',
+  templateUrl: './classpage.component.html',
+  styleUrls: ['./classpage.component.scss']
+})
+export class ClasspageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
