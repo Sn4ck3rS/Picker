63d0d53d1c0a2adb039b1232d7de27ca7fd18ca9
Axt <justin.axt@atos.net>
2020-06-04 10:26:15 +0200

    new http requests

--- src/app/homepage/drag-and-drop/drag-and-drop.component.ts
+++ src/app/homepage/drag-and-drop/drag-and-drop.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Student } from '../../../assets/Classes/student';
-import { HttpClient, HttpHeaders} from "@angular/common/http"
+import { HttpClient, HttpHeaders } from "@angular/common/http"
 
 @Component({
   selector: 'app-drag-and-drop',
@@ -9,107 +9,133 @@ import { HttpClient, HttpHeaders} from "@angular/common/http"
 })
 export class DragAndDropComponent implements OnInit {
 
-  constructor(private http: HttpClient, private header: HttpHeaders) { }
+  constructor(private http: HttpClient) { }
 
-  
+
   ngOnInit(): void {
-    
+    this.onLoadClasses();
   }
 
-  public students: any[] = []; 
-  fileToUpload: File = null; 
-  files:File[]=new Array<File>();
+  public students: Student[] = [];
+  fileToUpload: File = null;
+  files: File[] = new Array<File>();
   fd: FormData;
   fileName: string;
 
-  
-  @ViewChild('csvReader') csvReader: any;  
-  
-  uploadListener($event: any): void {  
-  
-    let files = $event.srcElement.files; 
-    this.files=files;
-    this.fileToUpload = $event.target.files[0];
-    this.fileName=this.fileToUpload.name;
+  @ViewChild('csvReader') csvReader: any;
 
-  
-    if (this.isValidCSVFile(files[0])) {  
-  
-      let input = $event.target;  
-      let reader = new FileReader();  
-      reader.readAsText(input.files[0]);  
+  uploadListener($event: any): void {
+
+    let files = $event.srcElement.files;
+    this.files = files;
+    this.fileToUpload = $event.target.files[0];
+    this.fileName = this.fileToUpload.name;
+
+
+    if (this.isValidCSVFile(files[0])) {
+
+      let input = $event.target;
+      let reader = new FileReader();
+      reader.readAsText(input.files[0]);
 
       console.log(this.fileToUpload)
-  
-      reader.onload = () => {  
-        let csvData = reader.result;  
-        let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);  
-  
-        let headersRow = this.getHeaderArray(csvRecordsArray);  
-  
-        this.students = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);  
-      };  
-  
-      reader.onerror = function () {  
-        console.log('Beim Lesen der Datei ist ein Fehler aufgetreten');  
-      };  
-      
-  
-    } else {  
-      alert("Bitte wählen sie eine gültige .csv-Datei .");  
-      this.fileReset();  
-    }  
-  }  
-  
-  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {  
-    let csvArr = [];  
-  
-    for (let i = 0; i < csvRecordsArray.length; i++) { 
-      
-      
-      let currentRecord = (csvRecordsArray[i]).split(';');  
-      
-      if (currentRecord.length == headerLength) {  
-        let csvRecord: Student = new Student();  
-        csvRecord.class = currentRecord[0];  
-        csvRecord.firstName = currentRecord[1];  
-        csvRecord.lastName = currentRecord[2];  
-        csvArr.push(csvRecord);  
-      }  
-    }  
-    return csvArr;  
-  }  
-  
-  isValidCSVFile(file: any) {  
-    return file.name.endsWith(".csv");  
-  }  
-  
-  getHeaderArray(csvRecordsArr: any) {  
-    let headers = (<string>csvRecordsArr[0]).split(';');  
-    let headerArray = [];  
-    for (let j = 0; j < headers.length; j++) {  
-      headerArray.push(headers[j]);  
-    }  
-    return headerArray;  
-  }  
-  
-  fileReset() {  
-    this.csvReader.nativeElement.value = "";  
-    this.students = [];  
-  }  
 
-  onUpload(){
-    let formData = new FormData();
-    for (var i = 0; i < this.files.length; i++) {
-        formData.append('upload[]', this.files[i], this.files[i].name);
-        console.log(this.files[i].name);
+      reader.onload = () => {
+        let csvData = reader.result;
+        let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);
+
+        let headersRow = this.getHeaderArray(csvRecordsArray);
+
+        this.students = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+      };
+
+      reader.onerror = function () {
+        console.log('Beim Lesen der Datei ist ein Fehler aufgetreten');
+      };
+
+
+    } else {
+      alert("Bitte wählen sie eine gültige .csv-Datei .");
+      this.fileReset();
     }
-    this.header.append("filename", this.files[i].name)
-    console.log(formData.get('upload[]'));
-    this.http.post('/api/upload', formData, {headers: this.header})
-    .subscribe((response) => {
-         console.log('response received is ', response);
+  }
+
+  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {
+    let csvArr = [];
+
+    for (let i = 0; i < csvRecordsArray.length; i++) {
+
+
+      let currentRecord = (csvRecordsArray[i]).split(';');
+
+      if (currentRecord.length == headerLength) {
+        let csvRecord: Student = new Student();
+        csvRecord.class = currentRecord[0];
+        csvRecord.firstName = currentRecord[1];
+        csvRecord.lastName = currentRecord[2];
+        csvArr.push(csvRecord);
+      }
+    }
+    return csvArr;
+  }
+
+  isValidCSVFile(file: any) {
+    return file.name.endsWith(".csv");
+  }
+
+  getHeaderArray(csvRecordsArr: any) {
+    let headers = (<string>csvRecordsArr[0]).split(';');
+    let headerArray = [];
+    for (let j = 0; j < headers.length; j++) {
+      headerArray.push(headers[j]);
+    }
+    return headerArray;
+  }
+
+  fileReset() {
+    this.csvReader.nativeElement.value = "";
+    this.students = [];
+  }
+
+  createJson(): any {
+
+    let studentData = new Array<any>();
+    this.students.forEach(s => {
+      let student = {
+        classDesc: s.class,
+        name: s.lastName,
+        firstName: s.firstName
+      }
+      studentData.push(student)
     })
+
+    return JSON.stringify(studentData);
+
+  }
+  onLoadClasses() {
+    this.http.get('/api/classes').subscribe(data => console.log("dis is ma data" + data))
+  }
+
+  onClassChosen(chosenClass: string){
+    this.http.post('/api/classInfo', {cl: chosenClass}).subscribe(data => console.log("studentdata: " + data))
+  }
+
+
+
+  onUpload() {
+
+    // let formData = new FormData();
+    // for (var i = 0; i < this.files.length; i++) {
+    //     formData.append('upload[]', this.files[i], this.students[i].class + ".json");
+
+    // }
+    // this.header.append("filename", this.files[i].name)
+    this.onClassChosen("FS172");
+
+    this.http.post('/api/upload', { students: this.students, name: this.students[0].class + ".json" }) //JSON
+      .subscribe((response) => {
+        console.log('response received is ', response);
+      })
   }
 
 }
